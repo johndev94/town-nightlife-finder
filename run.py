48d1cbad2f72f028937375a19b4d212be71b817e
John Devaney <johndevaney1@hotmail.com>
2026-04-09 17:23:36 +0100

    Server added
    Added server capabilities

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import os
+
 from app import create_app
 
 
@@ -5,4 +7,7 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    host = os.environ.get("FLASK_RUN_HOST", "0.0.0.0")
+    port = int(os.environ.get("FLASK_RUN_PORT", "5000"))
+    debug = os.environ.get("FLASK_DEBUG", "1").lower() in {"1", "true", "yes", "on"}
+    app.run(host=host, port=port, debug=debug)
